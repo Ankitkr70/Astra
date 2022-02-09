39b25f7e9326fb6b39a7b43309c14dda9c0d5ce0
Ankit Kumar <ankitkr70@gmail.com>
2022-02-09 23:22:08 +0530

    Added the complete Project

---
+++ script.js
@@ -0,0 +1,10 @@
+const showcase = document.querySelector(".showcase-text");
+const form = document.querySelector(".showcase-form");
+const stats = document.querySelector(".stats .container");
+console.log(stats);
+
+(function () {
+  showcase.classList.add("showcase-slide");
+  form.classList.add("showcase-slide");
+  stats.classList.add("stats-slide");
+})();
